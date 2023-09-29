bcc2dff70bd07cab4d0182cf1581733840cdd20b
Lindorm236 <elielbjean@gmail.com>
2023-09-29 14:54:43 +0000

    init

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+    <head></head>
+    <body>
+<form method="post" action="formResult">
+    <label for="firstName">Nom :</label><input type="text" id="firstName" name="firstName">
+</form>
+</body>
+</html>
